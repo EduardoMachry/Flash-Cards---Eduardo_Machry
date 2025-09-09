c60f2db7377b8d53f8eb0bce382dabbec6c26b52
Dudu <eduardo.machry@escola.pr.gov.br>
2025-09-09 08:46:07 -0300

    09/09/2025

---
+++ Perguntas.js
@@ -0,0 +1,67 @@
+Ex:
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual é o autor de "Dom Quixote"?',
+    'Miguel de Cervantes.'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
